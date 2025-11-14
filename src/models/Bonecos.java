84c994ca688b5b03b50c2a327aae1373dd74a281
Caio Ximenes <caio.ximenesmg@gmail.com>
2025-11-14 16:13:33 -0300

    Classes

---
+++ src/models/Bonecos.java
@@ -0,0 +1,16 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public abstract class Bonecos {
+    Tabuleiro tabuleiro;
+    ArrayList<Objects> coordenadas;
+    int tamanho;
+    Player player;
+    ArrayList<Object> tirosRecebidos;
+
+    public abstract boolean receberTiro(Ponto ponto);
+
+    public abstract boolean abatido();
+
